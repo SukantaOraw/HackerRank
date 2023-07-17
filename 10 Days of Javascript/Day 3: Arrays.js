b8124c218fee3d955500160cd0381b292448461c
Sukanta Oraw <sukantaoraw800@gmail.com>
2023-07-17 18:10:30 +0530

    Update Day 3: Arrays.js

--- 10 Days of Javascript/Day 3: Arrays.js
+++ 10 Days of Javascript/Day 3: Arrays.js
@@ -29,3 +29,56 @@
 // Explanation 0
 // Given the array nums = [2, 3, 6, 6, 5], we see that the largest value in the array is 6 and the second largest value is 5. Thus, we return 5 as our answer.
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let firstLargestNum = 0;
+  let secondLargestNum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > firstLargestNum) {
+      secondLargestNum = firstLargestNum;
+      firstLargestNum = nums[i];
+    }
+    if (nums[i] > secondLargestNum && nums[i] < firstLargestNum) {
+      secondLargestNum = nums[i];
+    }
+  }
+  return secondLargestNum;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
