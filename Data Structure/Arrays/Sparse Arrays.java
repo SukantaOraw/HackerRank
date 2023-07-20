39a26ad18082aea11a5e474be1f371d7f0f47eb5
Sukanta Oraw <sukantaoraw800@gmail.com>
2023-07-21 00:48:23 +0530

    Create Sparse Arrays.java

---
+++ Data Structure/Arrays/Sparse Arrays.java
@@ -0,0 +1,156 @@
+// Problem Statement :
+
+// There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+
+// Example:
+// strings=['ab', 'ab', 'abc']
+// queries=['ab', 'abc', 'bc']
+
+// There are  instances of 'ab', 1 of 'abc' and 0 of 'bc'. For each query, add an element to the return array, results=[2,1,0].
+
+// Function Description:
+// Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+// matchingStrings has the following parameters:
+
+//    1. string strings[n] - an array of strings to search
+//    2. string queries[q] - an array of query strings
+
+// Returns:
+//      1. int[q]: an array of results for each query
+
+// Input Format:
+// The first line contains and integer n, the size of strings[].
+// Each of the next n lines contains a string strings[i].
+// The next line contains q, the size of queries[].
+// Each of the next q lines contains a string queries[i].
+
+// Constraints:
+//    1. 1<=n<=1000 
+//    2. 1<=q<=1000
+//    3. 1<=strings[i],queries[i]<=20.
+
+// Sample Input 1
+// Array: stringList
+// aba
+// baba
+// aba
+// xzxb
+
+// Array: queries
+// aba
+// xzxb
+// ab
+ 
+// 4
+// aba
+// baba
+// aba
+// xzxb
+// 3
+// aba
+// xzxb
+// ab
+  
+// Sample Output 1
+// 2
+// 1
+// 0
+  
+// Explanation 1
+// Here, "aba" occurs twice, in the first and third string. The string "xzxb" occurs once in the fourth string, and "ab" does not occur at all.
+
+// Sample Input 2
+
+// Array: stringList
+// def
+// de
+// fgh
+
+// Array: queries
+// de
+// lmn
+// fgh
+ 
+// 3
+// def
+// de
+// fgh
+// 3
+// de
+// lmn
+// fgh
+  
+// Sample Output 2
+// 1
+// 0
+// 1
+
+// Sample Input 3
+
+// Array: stringList
+// abcde
+// sdaklfj
+// asdjf
+// na
+// basdn
+// sdaklfj
+// asdjf
+// na
+// asdjf
+// na
+// basdn
+// sdaklfj
+// asdjf
+
+// Array: queries
+// abcde
+// sdaklfj
+// asdjf
+// na
+// basdn
+
+// 13
+// abcde
+// sdaklfj
+// asdjf
+// na
+// basdn
+// sdaklfj
+// asdjf
+// na
+// asdjf
+// na
+// basdn
+// sdaklfj
+// asdjf
+// 5
+// abcde
+// sdaklfj
+// asdjf
+// na
+// basdn
+  
+// Sample Output 3
+// 1
+// 3
+// 4
+// 3
+// 2
+
+public static List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
+
+        // Write your code here
+        List<Integer> result = new ArrayList<>();
+
+        for (String query : queries) {
+            int count = 0;
+            for (String str : stringList) {
+                if (str.equals(query)) {
+                    count++;
+                }
+            }
+            result.add(count);
+        }
+
+        return result;
+    }
